a7e269c8982c92ac2884b8a79f6baf17b88f761e
Kir Proto Q <kir.proto@ya.ru>
2022-02-10 05:51:59 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Alerts__and_action/Yan_SwiftUI_Alerts__and_actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Alerts__and_actionApp.swift
+//  Yan_SwiftUI_Alerts__and_action
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Alerts__and_actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
